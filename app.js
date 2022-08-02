a5b1d52079841a42745f00edd5d6f8df6f066c86
Юрій <yurijlitviniii@gmail.com>
2022-08-02 15:45:52 +0300

    Add styles.css, public and css folders, app.js: add use static files

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 let items = [];
 
