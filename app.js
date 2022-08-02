e6bbce2116cb7df97137a6061625f82a6f35b254
Юрій <yurijlitviniii@gmail.com>
2022-08-02 16:46:45 +0300

    list.ejs: Add link to styles.css and beautify html tamplate

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let items = [];
+let items = ["Get up early"];
 
 app.get("/", function (req, res) {
   let today = new Date();
